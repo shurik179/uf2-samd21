55c46b864e4a562fb4b5fb7423861d4f0735e10c
shurik179 <shurik179@gmail.com>
2023-11-03 20:00:03 -0400

    added yozh board

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-BOARD=zero
+BOARD=yozh
 -include Makefile.user
 include boards/$(BOARD)/board.mk
 CC=arm-none-eabi-gcc
---
+++ boards/yozh/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define __SAMD21G18A__ 1
+
+#define VENDOR_NAME "Island Robotics."
+#define PRODUCT_NAME "Yozh Robot"
+#define VOLUME_LABEL "YOZH"
+
+#define USB_VID 0x1d50   // OpenMoko
+#define USB_PID 0x6186   // requested PID
+
+#define BOARD_ID "SAMD21G18A-Yozh"
+
+#define LED_PIN PIN_PA09
+
+
+#endif
